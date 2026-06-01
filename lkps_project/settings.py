a9c9177eaf0d50d0939671db5af71e8f27fd8e6e
JovanVito <jovanvitohd@gmail.com>
2026-06-01 14:57:18 +0700

    FNAL UPDATE

--- lkps_project/settings.py
+++ lkps_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,7 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'  # Pastikan ada garis miring di depan dan belakang
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')# Pastikan ada garis miring di depan dan belakang
 
 # TANDA KURUNG SIKU INI WAJIB ADA
 STATICFILES_DIRS = [
